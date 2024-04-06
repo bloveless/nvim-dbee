22737e74e800db311d20e5a2d219890da33139a6
Brennon Loveless <brennon.loveless@gmail.com>
2024-04-05 22:20:23 -0700

    fix: comment out unused variable

--- dbee/adapters/spanner.go
+++ dbee/adapters/spanner.go
@@ -33,17 +33,17 @@ func (s *Spanner) Connect(url string) (core.Driver, error) {
 }
 
 func (*Spanner) GetHelpers(opts *core.TableOptions) map[string]string {
-	basicConstraintQuery := `
-	SELECT tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name, rc.update_rule, rc.delete_rule
-	FROM
-		information_schema.table_constraints AS tc
-		JOIN information_schema.key_column_usage AS kcu
-			ON tc.constraint_name = kcu.constraint_name
-		JOIN information_schema.referential_constraints as rc
-			ON tc.constraint_name = rc.constraint_name
-		JOIN information_schema.constraint_column_usage AS ccu
-			ON ccu.constraint_name = tc.constraint_name
-	`
+	// basicConstraintQuery := `
+	// SELECT tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name, rc.update_rule, rc.delete_rule
+	// FROM
+	// 	information_schema.table_constraints AS tc
+	// 	JOIN information_schema.key_column_usage AS kcu
+	// 		ON tc.constraint_name = kcu.constraint_name
+	// 	JOIN information_schema.referential_constraints as rc
+	// 		ON tc.constraint_name = rc.constraint_name
+	// 	JOIN information_schema.constraint_column_usage AS ccu
+	// 		ON ccu.constraint_name = tc.constraint_name
+	// `
 
 	return map[string]string{
 		"List":         fmt.Sprintf("SELECT * FROM %s LIMIT 500", opts.Table),
